584c2c071d84b3e5610e2d27cf694b8c83916e74
r-uu <roger.wegner@web.de>
2024-07-05 08:10:30 +0200

    large reorganisation

--- gen/java/fx/bean.editor.demo/src/main/java/module-info.java
+++ gen/java/fx/bean.editor.demo/src/main/java/module-info.java
@@ -2,14 +2,21 @@ module de.ruu.lib.gen.java.fx.bean.editor.demo
 {
 	exports de.ruu.lib.gen.java.fx.bean.editor.demo;
 
-	requires javafx.base;
-	requires static lombok;
 	requires com.tngtech.archunit;
+
+	requires static lombok;
+	requires org.apache.logging.log4j;
+	requires org.slf4j;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+
+	requires de.ruu.lib.fx.comp;
 	requires de.ruu.lib.gen.core;
 	requires de.ruu.lib.gen.java;
 	requires de.ruu.lib.gen.java.fx.bean;
 	requires de.ruu.lib.gen.java.fx.comp;
-	requires de.ruu.lib.fx.comp;
-	requires javafx.fxml;
 	requires de.ruu.lib.gen.java.fx.bean.editor;
 }
