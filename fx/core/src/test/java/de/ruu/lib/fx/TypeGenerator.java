59e99c3870a05badbaf555ebe4b5fb003e8cf783
r-uu <roger.wegner@web.de>
2025-07-18 06:00:13 +0200

    in progress

--- fx/core/src/test/java/de/ruu/lib/fx/TypeGenerator.java
+++ fx/core/src/test/java/de/ruu/lib/fx/TypeGenerator.java
@@ -9,36 +9,41 @@ import java.io.IOException;
 @Slf4j
 public class TypeGenerator
 {
-	public static void main(String[] args) throws IOException, GeneratorException
-	{
-		generateJavaFXComponentsButtonViews();
-	}
+	public static void main(String[] args) throws IOException, GeneratorException { generate(); }
 
-	private static void generateJavaFXComponentsButtonViews() throws IOException, GeneratorException
+	private static void generate() throws IOException, GeneratorException
 	{
 		GeneratorFXCompBundle generator;
-		log.debug("create java fx component bundle for add button view");
-		generator =
-				new GeneratorFXCompBundle
-						(
-								"de.ruu.lib.fx.control.buttons",
-								"Add"
-						);
-		generator.run();
-		log.debug("create java fx component bundle for remove button view");
-		generator =
-				new GeneratorFXCompBundle
-						(
-								"de.ruu.lib.fx.control.buttons",
-								"Remove"
-						);
-		generator.run();
+//		log.debug("create java fx component bundle for add button view");
+//		generator =
+//				new GeneratorFXCompBundle
+//						(
+//								"de.ruu.lib.fx.control.buttons",
+//								"Add"
+//						);
+//		generator.run();
+//		log.debug("create java fx component bundle for remove button view");
+//		generator =
+//				new GeneratorFXCompBundle
+//						(
+//								"de.ruu.lib.fx.control.buttons",
+//								"Remove"
+//						);
+//		generator.run();
+//		log.debug("create java fx component bundle for edit button view");
+//		generator =
+//				new GeneratorFXCompBundle
+//						(
+//								"de.ruu.lib.fx.control.buttons",
+//								"Edit"
+//						);
+//		generator.run();
 		log.debug("create java fx component bundle for edit button view");
 		generator =
 				new GeneratorFXCompBundle
 						(
-								"de.ruu.lib.fx.control.buttons",
-								"Edit"
+								"de.ruu.app.jeeeraaah.client.fx.task.view.hierarchy",
+								"TaskHierarchyAbstract"
 						);
 		generator.run();
 	}
