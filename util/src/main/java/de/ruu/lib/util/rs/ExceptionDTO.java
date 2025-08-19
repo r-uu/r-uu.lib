8d87066b5a6fb96587eb771c55dca0cc58fcdd32
r-uu <roger.wegner@web.de>
2025-08-19 12:25:06 +0200

    Create ExceptionDTO.java

---
+++ util/src/main/java/de/ruu/lib/util/rs/ExceptionDTO.java
@@ -0,0 +1,14 @@
+package de.ruu.lib.util.rs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Getter @Accessors(fluent = true)
+@NoArgsConstructor @AllArgsConstructor
+public class ExceptionDTO
+{
+	private String message;
+	private String cause;
+}
