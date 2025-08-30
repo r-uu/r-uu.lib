a366256e6bd7eb68c5b9d40a0d83ccbc96e38808
r-uu <roger.wegner@web.de>
2025-08-30 10:13:48 +0200

    Update IO.java

--- util/src/main/java/de/ruu/lib/util/IO.java
+++ util/src/main/java/de/ruu/lib/util/IO.java
@@ -1,15 +1,19 @@
 package de.ruu.lib.util;
 
+import lombok.NonNull;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public interface IO
 {
-	public static String toString(InputStream inputStream) throws IOException
+	public static String toString(@NonNull InputStream inputStream) throws IOException
 	{
 		ByteArrayOutputStream result = new ByteArrayOutputStream();
 
@@ -22,18 +26,57 @@ public interface IO
 			result.write(buffer, 0, length);
 		}
 
-		return result.toString(StandardCharsets.UTF_8.name());
+		return result.toString(UTF_8);
 	}
 
-	public static boolean isListening(String host, int port) throws UnknownHostException
+	public static boolean isListening(@NonNull String host, int port) throws UnknownHostException
 	{
-		try (Socket socket = new Socket(host, port))
+		try (Socket socket = new Socket(host, port)) { return true;  }
+		catch (IOException e)                        { return false; }
+	}
+
+	/**
+	 * Captures everything that the given runnable writes to the given PrintStream (e.g. System.out or System.err),
+	 * restores the original stream, forwards the captured output back to the original stream, and returns the captured
+	 * output as a string.
+	 *
+	 * @param printStream the PrintStream to capture (e.g. System.out or System.err)
+	 * @param runnable    the code to execute
+	 * @return            the captured output as string
+	 */
+	public static String capturePrintStreamOutputOfRunnable(@NonNull PrintStream printStream, @NonNull Runnable runnable)
+	{
+		PrintStream original = printStream; // save original stream
+
+		// capture output in memory
+		ByteArrayOutputStream capturingStreamAsBinaryArrayOutputStream =
+				new ByteArrayOutputStream();
+		PrintStream           capturingStreamAsPrintStream =
+				new PrintStream(capturingStreamAsBinaryArrayOutputStream, true);
+
+		try
 		{
-			return true;
+			// temporarily redirect System.out or System.err if needed
+			if      (printStream == System.out) System.setOut(capturingStreamAsPrintStream);
+			else if (printStream == System.err) System.setErr(capturingStreamAsPrintStream);
+			// for custom PrintStream, assign capturing stream
+			else                                printStream = capturingStreamAsPrintStream;
+
+			// execute the runnable, capturing everything it writes to the p stream
+			runnable.run();
 		}
-		catch (IOException e)
+		finally
 		{
-			return false;
+			// restore original stream
+			if      (original == System.out) System.setOut(original);
+			else if (original == System.err) System.setErr(original);
 		}
+
+		String result = capturingStreamAsBinaryArrayOutputStream.toString();
+
+		// forward result output to the original stream
+		original.print(result);
+
+		return result;
 	}
 }
